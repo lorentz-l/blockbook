d6faec38407afd5458983beff6536c55d09b1245
lorentz-l <lorentz.l@proton.me>
2024-11-13 23:24:21 +0800

    fix: getBlockWorker 3 connect block error Block not found. Retrying...

--- db/sync.go
+++ db/sync.go
@@ -329,7 +329,7 @@ func (w *SyncWorker) ConnectBlocksParallel(lower, higher uint32) error {
 	GetBlockLoop:
 		for hh := range hch {
 			for {
-				block, err = w.chain.GetBlock(hh.hash, hh.height)
+				block, err = w.chain.GetBlock("", hh.height)
 				if err != nil {
 					// signal came while looping in the error loop
 					if hchClosed.Load() == true {
